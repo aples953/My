36ac1dcc31106c703dcfe98967c8d1d73f006371
aples953 <119748327+aples953@users.noreply.github.com>
2023-04-11 00:06:53 +0200

    ssgjnfdc
    qqqqqexcvgf

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ admins = {}
 load_dotenv()
 
 # client vars
-API_ID = int(getenv("API_ID", "7452578")) #لا تغير هاذة القيمة
-API_HASH = getenv("API_HASH", "061d67ee8eed9368c5cadabb4aa21efc")#لا تغير هاذة القيمة
+API_ID = int(getenv("API_ID", "26677661")) #لا تغير هاذة القيمة
+API_HASH = getenv("API_HASH", "695e9a1967eae770b5c8deed106763f0")#لا تغير هاذة القيمة
 BOT_TOKEN = getenv("BOT_TOKEN", "5957163344:AAF0lYYgxAN07BIBPZO9iHBg93_DXwLCBvc")
 SESSION_NAME = getenv("SESSION_NAME", "BAC-0L17JGlvhYpAHamJgF4dS3WYs6Pg8gS2w-ReKabLJhyeXy8X-V0uWU5U7v5RHev-cNbc9KlefU4MZnMFCHUPCfEh46EDVuE6LYixT1gKHjtjqHujoXP_507kanATJgjlw2qkO-j0oT29gndV2KPdxq8aJ1r_i1gDUnuGnlbfUfWy_MDC4866in8dI6eaXPygLQVDNzOU9cTOxF9j1g_qNBox2fhlYgWinrDf_GylGcyN1GDFREuoRuT4oWMimWWPwee3LGmVBKWd-meaW75857IV6FMYD8LttySM9OetB5rLGSr9vFQzXsptqSJKHOBeZmS7A2xKGboxXL8pD_23AAAAAWvi6HAA")
 
@@ -25,7 +25,7 @@ MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mong
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . $").split())
 OWNER_ID = list(map(int, getenv("OWNER_ID", "5889544325").split()))
                                              #هنا ضع ايدي المطور فوق و الاعلئ
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1854384004").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5889544325").split()))
 
 # image resources vars
 IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/2a726c634dbc3b9e8f451.png")
